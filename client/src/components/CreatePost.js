ee57bbe5aa861971469511aca52de5fec46f51bd
Kaustav Shouvik <kaustav.shouvik00@gmail.com>
2020-09-09 18:01:20 +0530

    complete kubernetes cluster

--- client/src/components/CreatePost.js
+++ client/src/components/CreatePost.js
@@ -11,7 +11,7 @@ function CreatePost() {
     const handleSubmit = async e => {
         e.preventDefault();
         
-        await axios.post('http://localhost:4000/posts', {
+        await axios.post('http://posts.com/posts/create', {
             title
         })
 
